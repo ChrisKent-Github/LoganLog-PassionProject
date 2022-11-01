f67a495b6cea9437fc59c4711de3300296fb7819
ChrisKent-Github <chris_kent@ymail.com>
2022-11-01 19:00:21 -0400

    resources folder

---
+++ src/main/java/com/github/ChrisKentGithub/LoganLog/LoganLogApplication.java
@@ -0,0 +1,13 @@
+package com.github.ChrisKentGithub.LoganLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoganLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoganLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ChrisKentGithub/LoganLog/LoganLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ChrisKentGithub.LoganLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoganLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
